bcf14bd609b589678d354cb30ca02e4c2e031c0f
Tim Ocampo <ocampo@Tims-MacBook-Pro.local>
2024-04-29 20:29:44 -0700

    added controllers for User, getAllUsers and getSingleUser

---
+++ server/controllers/userControllers.js
@@ -0,0 +1,88 @@
+const UserModel = require("../models/User");
+
+/* Grab all Users */
+
+/*  -----  Create a Single User ------ */
+const createNewUser = async (req, res) => {
+  try {
+    const newUser = {
+      name: req.body?.name,
+      username: req.body?.username,
+      email: req.body?.email,
+    };
+
+    const response = await UserModel.create({
+      ...newUser,
+      address: {
+        street: req.body?.street ? req.body?.street : "",
+        city: req.body?.city ? req.body?.city : "",
+        zipcode: req.body?.zipcode ? req.body?.zipcode : "",
+      },
+      phone: req.body?.phone ? req.body?.phone : "",
+    });
+
+    res.send({
+      id: response._id,
+      name: response.name,
+      email: response.email,
+      address: {
+        ...response.address,
+      },
+      phone: response.phone,
+      createdAt: response.createdAt,
+      updatedAt: response.updatedAt,
+    });
+  } catch (err) {
+    res.json({
+      message: `Failed to add user - ${err.message}`,
+    });
+  }
+};
+
+/* -----    Delete a user -----*/
+
+/*----- update a new User -----  */
+
+/*----- Get a single new User -----  */
+
+const getSingleUser = async (req, res) => {
+  console.log("verify single user", req.params);
+
+  const { id } = req.params;
+
+  try {
+    const User = await UserModel.findById(id);
+
+    if (!User) {
+      return res.send({ message: "No User found" });
+    }
+
+    return res.send({
+      message: "User found!",
+      user: User,
+    });
+  } catch (error) {
+    res.send({message: 'getSingleUser - Error connecting to database'})
+  }
+};
+
+/*----- Get ALL Users -----  */
+const getAllUsers = async (req, res) => {
+  try {
+    const allUsers = await UserModel.find({}).sort({ createdAt: -1 });
+    return res.send({
+      message: "SUCCESS - retrieved all users",
+      users: allUsers,
+    });
+  } catch (error) {
+    return res.send({
+      message: "Failed to grab all users",
+    });
+  }
+};
+
+module.exports = {
+  createNewUser,
+  getAllUsers,
+  getSingleUser,
+};
--- server/routes/User.js
+++ server/routes/User.js
@@ -1,52 +1,21 @@
 const express = require("express");
 
 const router = express.Router();
-const UserModel = require("../models/User");
 
-router.get("/", (req, res) => {
-  res.send("USERS-from-router");
-});
+const {
+  getAllUsers,
+  getSingleUser,
+  createNewUser,
+} = require("../controllers/userControllers");
 
-router.get("/:id", (req, res) => {
-  res.send("grab user by id");
-});
+/* Get a Single User */
+router.get("/:id", getSingleUser);
+
+/* Get All Users */
+router.get("/", getAllUsers);
 
 /* Add User */
-router.post("/", async (req, res) => {
-  try {
-    const newUser = {
-      name: req.body?.name,
-      username: req.body?.username,
-      email: req.body?.email,
-    };
-
-    const response = await UserModel.create({
-      ...newUser,
-      address: {
-        street: req.body?.street ? req.body?.street : "",
-        city: req.body?.city ? req.body?.city : "",
-        zipcode: req.body?.zipcode ? req.body?.zipcode : "",
-      },
-      phone: req.body?.phone ? req.body?.phone : "",
-    });
-
-    res.send({
-      id: response._id,
-      name: response.name,
-      email: response.email,
-      address: {
-        ...response.address,
-      },
-      phone: response.phone,
-      createdAt: response.createdAt,
-      updatedAt: response.updatedAt,
-    });
-  } catch (err) {
-    res.json({
-      message: `Failed to add user - ${err.message}`,
-    });
-  }
-});
+router.post("/", createNewUser);
 
 /*update user */
 router.patch("/:id", (req, res) => {
