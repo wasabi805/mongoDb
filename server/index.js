678bef22d0fd1c3a21fdcb9d5fc123e5100c1696
Tim Ocampo <ocampo@Tims-MacBook-Pro.local>
2024-04-29 17:21:50 -0700

    connected to mongoDb

--- server/index.js
+++ server/index.js
@@ -1,11 +1,23 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
-const port = 8080
+
+const mongoose = require("mongoose");
+
+
+
+mongoose.connect(process.env.MONGO_URI).then( then => {
+    app.listen( process.env.PORT, () => {
+        console.log(`Example app listening on port ${process.env.PORT}`)
+      })
+}).catch(err=>console.log('error connecting to mongo', err))
+
+
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    console.log( process.env.TEST_ME, 'yee')
+    
+  res.send('Hello World')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+
