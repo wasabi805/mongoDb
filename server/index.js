0ebfd56c65618359cf9cfca853b47ac7ba73cfa0
Tim Ocampo <ocampo@Tims-MacBook-Pro.local>
2024-05-04 22:44:08 -0700

    added helmet, compression, and express-rate-limiter.

--- server/index.js
+++ server/index.js
@@ -2,12 +2,31 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 var cors = require("cors");
+const compression = require("compression");
+const helmet = require("helmet")
 
 const app = express();
 
 /* middleware */
 app.use(express.json());
 app.use(cors());
+app.use(compression());
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      "script-src": ["'self'", "code.jquery.com", "cdn.jsdelivr.net"],
+    },
+  }),
+);
+
+const RateLimit = require("express-rate-limit");
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 20,
+});
+// Apply rate limiter to all requests
+app.use(limiter);
+
 
 /* DB connect */
 mongoose
