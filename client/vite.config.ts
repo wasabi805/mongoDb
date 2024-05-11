054c04fe58027bbfff5d3aeae8837a3b2531f1c0
Tim Ocampo <ocampo@Tims-MacBook-Pro.local>
2024-05-10 17:19:10 -0700

    configuring vite.config for prod.

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,15 +1,28 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import dotenv from "dotenv";
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    MODE: `"${process.env.MODE}"`,
+    VITE_LOCAL_HOST: `"${process.env.VITE_LOCAL_HOST}"`,
+    VITE_PROD_URL: `"${process.env.VITE_PROD_URL}"`,
+    VITE_LOCAL_PORT: `"${process.env.VITE_LOCAL_PORT}"`,
+  },
+
   plugins: [react()],
   server: {
     port: 8082,
 
     proxy: {
       "/api": {
-        target: "http://localhost:8080",
+        // target: "http://localhost:8080",
+        target:
+          process.env.MODE === "development"
+            ? process.env.VITE_LOCAL_HOST
+            : process.env.VITE_PROD_URL,
         changeOrigin: true,
         secure: false,
       },
