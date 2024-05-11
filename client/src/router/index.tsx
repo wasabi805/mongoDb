8637e37566688c763729745f25b4d672c5c965fb
Tim Ocampo <ocampo@Tims-MacBook-Pro.local>
2024-05-10 20:39:27 -0700

    prod now works, adding priviate route back.

--- client/src/router/index.tsx
+++ client/src/router/index.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Home from "../pages/Home";
 import Users from "../pages/Users";
-// import { PrivateRoute } from "./PrivateRoute";
+import { PrivateRoute } from "./PrivateRoute";
 
 // return <Navigate to="/login" state={{ from: history.location }} />
 
@@ -14,9 +14,9 @@ export const AppRouter = () => {
         <Route
           path="/users"
           element={
-            // <PrivateRoute>
+            <PrivateRoute>
             <Users />
-            // </PrivateRoute>
+          </PrivateRoute>
           }
         />
       </Routes>
